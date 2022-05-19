1aa09918f7f6a076c59f603150e89e96ac526e2c
Conch <616850155@qq.com>
2022-05-19 22:29:20 +0800

    给User类加上了get/set方法

--- src/cn/conch/entity/User.java
+++ src/cn/conch/entity/User.java
@@ -3,4 +3,20 @@ package cn.conch.entity;
 public class User {
     private String name;
     private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
